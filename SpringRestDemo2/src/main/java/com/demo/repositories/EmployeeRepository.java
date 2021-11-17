f2bf7c1540010cf73b225527a7dd2d9f9f003ae6
Vishal <vishal@Vishals-MacBook-Pro.local>
2021-11-17 12:44:30 +0530

    -

--- SpringRestDemo2/src/main/java/com/demo/repositories/EmployeeRepository.java
+++ SpringRestDemo2/src/main/java/com/demo/repositories/EmployeeRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource()
+@RepositoryRestResource
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 	List<Employee> findAllByCity(String city);
